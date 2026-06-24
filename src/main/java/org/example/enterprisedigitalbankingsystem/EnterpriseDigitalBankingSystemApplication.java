22d43d12b6475351dc77004ceb4f8464170db8a9
hemantwint-hey <hemantwint@gmail.com>
2026-06-24 19:36:46 +0530

    Started new Project

---
+++ src/main/java/org/example/enterprisedigitalbankingsystem/EnterpriseDigitalBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.enterprisedigitalbankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseDigitalBankingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseDigitalBankingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/enterprisedigitalbankingsystem/EnterpriseDigitalBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.enterprisedigitalbankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseDigitalBankingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
