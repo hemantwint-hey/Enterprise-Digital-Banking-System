bfd681f4d176ce323aab7fb57be9cebb88f1d5a1
hemantwint-hey <hemantwint@gmail.com>
2026-07-11 20:22:09 +0530

    Added JwtUtil class for token creation and extracting Claims

--- src/main/java/org/example/enterprisedigitalbankingsystem/security/JwtUtil.java
+++ src/main/java/org/example/enterprisedigitalbankingsystem/security/JwtUtil.java
@@ -1,6 +1,7 @@
 package org.example.enterprisedigitalbankingsystem.security;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -31,5 +32,26 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis()+jwtExpiration))
                 .signWith(getSigninKey())
                 .compact();
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    public String extractEmail(String token){
+        return  extractAllClaims(token).getSubject();
+    }
+    public Date extractExpiration(String token){
+        return extractAllClaims(token).getExpiration();
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+    public boolean validateToken(String token, UserDetails userDetails){
+        String email = extractEmail(token);
+        return  email.equals(userDetails.getUsername() )&& !isTokenExpired(token);
     }
 }
