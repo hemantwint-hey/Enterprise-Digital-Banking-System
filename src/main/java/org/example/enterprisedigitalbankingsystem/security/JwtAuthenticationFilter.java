d588af3914868386c0c095d7a8ba9e909b6d0b7e
hemantwint-hey <hemantwint@gmail.com>
2026-07-12 18:51:35 +0530

    AddedjwtAuthentication Filter

--- src/main/java/org/example/enterprisedigitalbankingsystem/security/JwtAuthenticationFilter.java
+++ src/main/java/org/example/enterprisedigitalbankingsystem/security/JwtAuthenticationFilter.java
@@ -1,4 +1,8 @@
 package org.example.enterprisedigitalbankingsystem.security;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtAuthenticationFilter {
+
 }
--- src/main/java/org/example/enterprisedigitalbankingsystem/security/SecurityConfig.java
+++ src/main/java/org/example/enterprisedigitalbankingsystem/security/SecurityConfig.java
@@ -1,5 +1,4 @@
 package org.example.enterprisedigitalbankingsystem.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,6 +8,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
         throws Exception{
