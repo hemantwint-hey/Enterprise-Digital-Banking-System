bdb01f4632804e0ff2a735eb546ac5b6cdc3f525
hemantwint-hey <hemantwint@gmail.com>
2026-07-14 20:48:14 +0530

    AddedjwtAuthentication Filter

--- src/main/java/org/example/enterprisedigitalbankingsystem/security/JwtAuthenticationFilter.java
+++ src/main/java/org/example/enterprisedigitalbankingsystem/security/JwtAuthenticationFilter.java
@@ -1,7 +1,7 @@
 package org.example.enterprisedigitalbankingsystem.security;
 
 import org.springframework.stereotype.Component;
-
+// here we will pply multiple filter
 @Component
 public class JwtAuthenticationFilter {
 
