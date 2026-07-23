87e74ab1e8396ee790ab285ad36b766aa1cdd977
hemantwint-hey <hemantwint@gmail.com>
2026-07-23 22:40:43 +0530

    added seperate exception handling

---
+++ src/main/java/org/example/enterprisedigitalbankingsystem/exception/ErrorResponse.java
@@ -0,0 +1,4 @@
+package org.example.enterprisedigitalbankingsystem.exception;
+
+public class ErrorResponse {
+}
---
+++ src/main/java/org/example/enterprisedigitalbankingsystem/exception/UnauthorizedException.java
@@ -0,0 +1,5 @@
+package org.example.enterprisedigitalbankingsystem.exception;
+
+public class UnauthorizedException {
+
+}
